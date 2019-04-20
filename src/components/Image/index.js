e61ae444d34e7fadba202154b133b3011731d908
Troy Chryssos <troychryssos@gmail.com>
2019-04-20 11:24:19 -0400

    built image component

---
+++ src/components/Image/index.js
@@ -0,0 +1,22 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import './styles.scss'
+
+const Image = (props) => {
+	const { src, size, alt } = props
+
+	return (
+		<div className={`imageWrapper image${size}`}>
+			<img src={src} alt={alt} className="imageImg" />
+		</div>
+	)
+}
+
+Image.propTypes = {
+	src: PropTypes.string,
+	alt: PropTypes.string,
+	size: PropTypes.oneOf(['full', 'half']),
+}
+
+export default Image
