78a2aa6038a220ce55be1fc23963804bfd98cb9e
Troy Chryssos <troychryssos@gmail.com>
2019-06-18 21:50:17 -0400

    tampons, musical rug page

--- src/pages/HowToTampons.js
+++ src/pages/HowToTampons.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import PageWrapper from 'components/PageWrapper'
+import PageContent from 'components/PageWrapper/components/PageContent'
+import PageDescription from 'components/PageWrapper/components/PageDescription'
 import ContentBlock from 'components/ContentBlock'
 import Heading from 'components/Typography/Heading'
 import Image from 'components/Image'
@@ -16,30 +18,32 @@ import TamponSeven from 'static/images/HowToTampons/Tampon-7.png'
 
 const HowToTampons = () => (
 	<PageWrapper>
-		<Spacer />
-		<ContentBlock>
-			<Heading>How to put in a tampon</Heading>
-			<Spacer />
-			<p>
-				How to Put in a Tampon is a collection of scrolling animations. Each interaction is based on a verb taken from a Young Women&apos;s Health article describing how to put in a tampon. This Piece was shown in the UCLA DMA 2016 Senior Exhibit, S.A.D. Show surrounded by tampons hand wrapped with the verbs used in the project.
-			</p>
-			<br />
-			<p>
-				Role: Creative Direction, Design, Computer Programming
-			</p>
-		</ContentBlock>
-		<Spacer />
-		<VideoPlayer
-			src="https://player.vimeo.com/video/331255354"
-			title="Tampon insertion animation"
-		/>
-		<Image src={TamponOne} alt="TV with tampon project" size="full" />
-		<Image src={TamponTwo} alt="Laptop with tampon project" size="full" />
-		<Image src={TamponThree} alt="Tampon opening animation" size="full" />
-		<Image src={TamponFour} alt="Flying tampon animation" size="full" />
-		<Image src={TamponFive} alt="Razor animation" size="full" />
-		<Image src={TamponSix} alt="Texting animation" size="full" />
-		<Image src={TamponSeven} alt="Heart flying out from between legs" size="full" />
+		<PageDescription>
+			<ContentBlock>
+				<Heading>How to put in a tampon</Heading>
+				<Spacer />
+				<p>
+					How to Put in a Tampon is a collection of scrolling animations. Each interaction is based on a verb taken from a Young Women&apos;s Health article describing how to put in a tampon. This Piece was shown in the UCLA DMA 2016 Senior Exhibit, S.A.D. Show surrounded by tampons hand wrapped with the verbs used in the project.
+				</p>
+				<br />
+				<p>
+					Role: Creative Direction, Design, Computer Programming
+				</p>
+			</ContentBlock>
+		</PageDescription>
+		<PageContent>
+			<VideoPlayer
+				src="https://player.vimeo.com/video/331255354"
+				title="Tampon insertion animation"
+			/>
+			<Image src={TamponOne} alt="TV with tampon project" size="full" />
+			<Image src={TamponTwo} alt="Laptop with tampon project" size="full" />
+			<Image src={TamponThree} alt="Tampon opening animation" size="full" />
+			<Image src={TamponFour} alt="Flying tampon animation" size="full" />
+			<Image src={TamponFive} alt="Razor animation" size="full" />
+			<Image src={TamponSix} alt="Texting animation" size="full" />
+			<Image src={TamponSeven} alt="Heart flying out from between legs" size="full" />
+		</PageContent>
 	</PageWrapper>
 )
 
--- src/pages/MusicalRug.js
+++ src/pages/MusicalRug.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import PageWrapper from 'components/PageWrapper'
+import PageContent from 'components/PageWrapper/components/PageContent'
+import PageDescription from 'components/PageWrapper/components/PageDescription'
 import ContentBlock from 'components/ContentBlock'
 import Heading from 'components/Typography/Heading'
 import Image from 'components/Image'
@@ -12,30 +14,32 @@ import MusicalRugThree from 'static/images/MusicalRug/MusicalRug-3.jpg'
 
 const UXUICourse = () => (
 	<PageWrapper>
-		<Spacer />
-		<ContentBlock>
-			<Heading>Musical Rug</Heading>
-			<Spacer />
-			<p>
-				Built using Arduino and Processing during UCLA|DMA&apos;s Winter 2016 Special Topics class with Instructor Casey Reas. The project combines hardware and software skills to create a rug that plays sounds upon touch. All sounds are also generated using code.
-			</p>
-			<br />
-			<p>
-				Role: Creative Direction, Design, Production
-			</p>
-		</ContentBlock>
-		<Spacer />
-		<VideoPlayer
-			src="https://player.vimeo.com/video/331255265"
-			title="Musical rug demo"
-		/>
-		<Image
-			src={MusicalRugOne}
-			alt="Two hands touching musical rug"
-			size="full"
-		/>
-		<Image src={MusicalRugTwo} alt="Setting up musical rug" size="full" />
-		<Image src={MusicalRugThree} alt="Touching musical rug" size="full" />
+		<PageDescription>
+			<ContentBlock>
+				<Heading>Musical Rug</Heading>
+				<Spacer />
+				<p>
+					Built using Arduino and Processing during UCLA|DMA&apos;s Winter 2016 Special Topics class with Instructor Casey Reas. The project combines hardware and software skills to create a rug that plays sounds upon touch. All sounds are also generated using code.
+				</p>
+				<br />
+				<p>
+					Role: Creative Direction, Design, Production
+				</p>
+			</ContentBlock>
+		</PageDescription>
+		<PageContent>
+			<VideoPlayer
+				src="https://player.vimeo.com/video/331255265"
+				title="Musical rug demo"
+			/>
+			<Image
+				src={MusicalRugOne}
+				alt="Two hands touching musical rug"
+				size="full"
+			/>
+			<Image src={MusicalRugTwo} alt="Setting up musical rug" size="full" />
+			<Image src={MusicalRugThree} alt="Touching musical rug" size="full" />
+		</PageContent>
 	</PageWrapper>
 )
 
