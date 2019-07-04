d36dc1a0dd4c3866b145c39298ef4aea3a74d88e
Troy Chryssos <troychryssos@gmail.com>
2019-07-04 11:41:39 -0400

    fixing object nesting in blobLink styles

--- src/components/BlobLink/styles.js
+++ src/components/BlobLink/styles.js
@@ -45,36 +45,36 @@ export default {
 			'&:last-of-type': {
 				paddingBottom: '1rem',
 			},
-			FISMarketing: {
-				transform: 'translateX(0)',
-			},
-			UXUICourse: {
-				transform: 'translate(-5vw, 5vw)',
-			},
-			MusicalRug: {
-				transform: 'translate(-2vw, 1vw)',
-			},
-			HowToTampons: {
-				transform: 'translate(-2vw, -0.5vw)',
-			},
-			TheUprisingCreative: {
-				transform: 'translate(-13vw, -4vw)',
-			},
-			SeatGeekScholarship: {
-				transform: 'translateY(-14vw)',
-			},
-			ChaseSapphire: {
-				transform: 'translateY(-23vw)',
-			},
-			FlatironSchoolStyleGuide: {
-				transform: 'translate(2vw, -23vw)',
-			},
-			How: {
-				transform: 'translate(-1vw, -25vw)',
-			},
-			ChangeThingsVideo: {
-				transform: 'translate(-5vw, -25vw)',
-			},
+		},
+		FISMarketing: {
+			transform: 'translateX(0)',
+		},
+		UXUICourse: {
+			transform: 'translate(-5vw, 5vw)',
+		},
+		MusicalRug: {
+			transform: 'translate(-2vw, 1vw)',
+		},
+		HowToTampons: {
+			transform: 'translate(-2vw, -0.5vw)',
+		},
+		TheUprisingCreative: {
+			transform: 'translate(-13vw, -4vw)',
+		},
+		SeatGeekScholarship: {
+			transform: 'translateY(-14vw)',
+		},
+		ChaseSapphire: {
+			transform: 'translateY(-23vw)',
+		},
+		FlatironSchoolStyleGuide: {
+			transform: 'translate(2vw, -23vw)',
+		},
+		How: {
+			transform: 'translate(-1vw, -25vw)',
+		},
+		ChangeThingsVideo: {
+			transform: 'translate(-5vw, -25vw)',
 		},
 	},
 }
