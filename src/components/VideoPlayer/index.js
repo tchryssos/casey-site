ec33c45c39b2876c61b5a8a474b5d366774ad944
Troy Chryssos <troychryssos@gmail.com>
2020-01-12 19:48:03 -0500

    video player

--- src/components/VideoPlayer/index.js
+++ src/components/VideoPlayer/index.js
@@ -1,13 +1,12 @@
 import React from 'react'
-import PropTypes from 'prop-types'
-import injectSheet from 'react-jss'
+import { createUseStyles } from 'react-jss'
 
-const styles = {
+const useStyles = createUseStyles(({ aspectRatio }) => ({
 	videoWrapper: {
 		marginTop: '16px',
 		position: 'relative',
 		width: '100%',
-		paddingBottom: ({ aspectRatio }) => {
+		paddingBottom: () => {
 			switch (aspectRatio) {
 				case '1:1':
 					return '100%'
@@ -25,24 +24,20 @@ const styles = {
 		width: '100%',
 		height: '100%',
 	},
+}))
+
+export default ({ src, title, aspectRatio }) => {
+	const classes = useStyles({ aspectRatio })
+	return (
+		<div className={classes.videoWrapper}>
+			<iframe
+				title={title}
+				src={src}
+				frameBorder="0"
+				allow="autoplay; fullscreen"
+				allowFullScreen
+				className={classes.video}
+			/>
+		</div>
+	)
 }
-
-const VideoPlayer = ({ src, title, classes }) => (
-	<div className={classes.videoWrapper}>
-		<iframe
-			title={title}
-			src={src}
-			frameBorder="0"
-			allow="autoplay; fullscreen"
-			allowFullScreen
-			className={classes.video}
-		/>
-	</div>
-)
-
-VideoPlayer.propTypes = {
-	src: PropTypes.string,
-	title: PropTypes.string,
-}
-
-export default injectSheet(styles)(VideoPlayer)
