4a19b3658b9ca686961622557328459b940b0847
Casey Bradford <caseyebradford@gmail.com>
2020-02-07 14:17:56 -0500

    elo page updated

--- src/pages/elo.js
+++ src/pages/elo.js
@@ -15,6 +15,9 @@ import OGELO from 'static/images/ELO/Og-elo.png'
 import FirstELO from 'static/images/ELO/first-elo.png'
 import ELOGif from 'static/images/ELO/elo-account-flow.gif'
 import ELOWhiteboard from 'static/images/ELO/elo-whiteboard.png'
+import ELODarkmode from 'static/images/ELO/elo-darkmode.gif'
+import ELOBluemode from 'static/images/ELO/elo-bluemode.png'
+import ELOYellow from 'static/images/ELO/elo-yellow.png'
 
 
 export default () => (
@@ -34,7 +37,7 @@ export default () => (
 		</PageDescription>
 		<PageContent>
 			<ContentBlock>
-				<SubHeading>VERSION 1</SubHeading>
+				<Heading>VERSION 1</Heading>
 				<Spacer />
 				<Body>
 					This is what my boyfriend had built on his own just for him and his friends to use together.
@@ -47,7 +50,7 @@ export default () => (
 				<Image size="full" src={FirstELO} />
 				<Spacer />
 				<Spacer />
-				<SubHeading>VERSION 2</SubHeading>
+				<Heading>VERSION 2</Heading>
 				<Spacer />
 				<Body>
 					We decided we wanted more of our friends and even strangers to be able to use the platform and use it for multiple games at a time. At this stage we created a flow to create an account and manage all of your scoreboards.
@@ -55,7 +58,7 @@ export default () => (
 				<Image size="full" src={ELOGif} />
 				<Spacer />
 				<Spacer />
-				<SubHeading>VERSION 3</SubHeading>
+				<Heading>VERSION 3</Heading>
 				<Spacer />
 				<Body>
 					At this point we recruited our friend who is a backend developer to help us make a more robust user experience. Here we created a brand new roadmap, mapping out all of the features we needed for an MVP.
@@ -70,11 +73,37 @@ export default () => (
 					title="Musical rug demo"
 				/>
 				<Spacer />
-				<SubHeading>BRANDING AND VISUAL DESIGN</SubHeading>
+				<Heading>BRANDING AND VISUAL DESIGN</Heading>
 				<Spacer />
 				<Body>
 					Once we felt comfortable with the wireframes and functionality, I explored a few visual directions for the platform.
 				</Body>
+				<Spacer />
+				<SubHeading>Direction 1</SubHeading>
+				<Spacer />
+				<Body>
+					Most gamers use dark mode so that they are not looking at bright screens at night and hurting their eyes. Most gaming-adjacent platforms that we looked at, such as Discord and Challoneg also primarily use a dark mode for their UI.
+				</Body>
+				<Image size="full" src={ELODarkmode} />
+				<Spacer />
+				<Spacer />
+				<SubHeading>Direction 2</SubHeading>
+				<Spacer />
+				<Body>
+					For a second direction I tried a blue mode so that we could keep the usability of a dark mode but stand out from out competitors a little more and add some more personality.
+				</Body>
+				<Image size="full" src={ELOBluemode} />
+				<Spacer />
+				<Spacer />
+				<SubHeading>Direction 3</SubHeading>
+				<Spacer />
+				<Body>
+					Here I wanted to try something very different and out of the box. I wanted to have something very playful since games are playful!
+				</Body>
+				<Image size="full" src={ELOYellow} />
+				<Spacer />
+				<Spacer />
+				<SubHeading>This project is ongoing! This is where we are at currently.</SubHeading>
 				<Spacer />
 				<Spacer />
 			</ContentBlock>
