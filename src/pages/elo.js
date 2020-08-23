d2996a623042adaf4fe042634229a276179d93ca
Casey Bradford <caseyebradford@gmail.com>
2020-08-23 12:31:41 -0400

    elo page nav

--- src/pages/elo.js
+++ src/pages/elo.js
@@ -1,6 +1,5 @@
 import React, { useRef } from 'react'
 import { createUseStyles } from 'react-jss'
-import clsx from 'clsx'
 
 import {
 	MD_MIN_STRING,
@@ -17,6 +16,7 @@ import Image from 'components/Image'
 import SubHeading from 'components/Typography/SubHeading'
 import ItemGrid from 'components/ItemGrid'
 import VideoPlayer from 'components/VideoPlayer'
+import PageNav from 'components/PageNav'
 
 import ELOStorybook from 'static/images/ELO/elo-storybook.png'
 import ELOAltLogo from 'static/images/ELO/elo-alt-logos.png'
@@ -86,10 +86,26 @@ const useStyles = createUseStyles({
 		width: '30%',
 		marginRight: 8,
 	},
+	sectionLabel: {
+		backgroundColor: '#4e7fff',
+		padding: 8,
+		color: 'white',
+		whiteSpace: 'nowrap',
+		textAlign: 'center',
+	},
 })
 
 export default () => {
 	const classes = useStyles()
+
+	const navLinks = [
+		{ link: 'brief' },
+		{ link: 'research' },
+		{ link: 'architecture' },
+		{ link: 'branding' },
+		{ link: 'testing' },
+		{ link: 'learnings' },
+	]
 
 	// START - PAGE SCROLL LOGIC - START
 	const scrollingContainer = useRef()
@@ -103,217 +119,258 @@ export default () => {
 
 	return (
 		<PageWrapper>
-			<ContentBlock className={classes.headerColor}>
-				<ItemGrid stackedOnMobile>
-					<div className={classes.half}>
-						<Heading>
-							Ladders allows people playing games together to track their ranking among the group
-						</Heading>
-						<Spacer />
-						<Spacer />
-						<SubHeading>
-							Role: Product Designer
-						</SubHeading>
-					</div>
-					<Image className={classes.half} src={Hero} />
-				</ItemGrid>
-				<Spacer />
-				<SubHeading>Main Goal</SubHeading>
-				<Spacer />
-				<Body>
-					Define and build out an MVP of the product that will allow people playing games together to track their ranking among the group.
-				</Body>
-			</ContentBlock>
-			<ContentBlock className={classes.whiteBlock}>
-				<Heading>Understanding the user</Heading>
-				<Spacer />
-				<Body>
-					I interviewed a handful of people who play games with freinds regularly and used their statements to create the following persona.
-				</Body>
-				<Spacer />
-				<div className={classes.persona}>
-					<ItemGrid>
+			<PageNav navLinkObjects={navLinks} />
+			{/* eslint-disable jsx-a11y/anchor-is-valid */}
+
+			{/* START - BRIEF - START */}
+			<a name="brief">
+				<ContentBlock className={classes.headerColor}>
+					<ItemGrid stackedOnMobile>
 						<div className={classes.half}>
-							<div className={classes.personaName}>
-								<Image src={Persona} className={classes.personaImage} />
-								<div>
-									<Heading>Sam</Heading>
-									<SubHeading>The Avid Gamer</SubHeading>
+							<Heading>
+								Ladders allows people playing games together to track their ranking among the group
+							</Heading>
+							<Spacer />
+							<Spacer />
+							<SubHeading>
+								Role: Product Designer
+							</SubHeading>
+						</div>
+						<Image className={classes.half} src={Hero} />
+					</ItemGrid>
+					<Spacer />
+					<SubHeading>Main Goal</SubHeading>
+					<Spacer />
+					<Body>
+						Define and build out an MVP of the product that will allow people playing games together to track their ranking among the group.
+					</Body>
+				</ContentBlock>
+			</a>
+
+			{/* START - RESEARCH - START */}
+			<a name="research">
+				<div className={classes.sectionLabel}>
+					RESEARCH
+				</div>
+				<ContentBlock className={classes.whiteBlock}>
+					<Heading>Understanding the user</Heading>
+					<Spacer />
+					<Body>
+						I interviewed a handful of people who play games with freinds regularly and used their statements to create the following persona.
+					</Body>
+					<Spacer />
+					<div className={classes.persona}>
+						<ItemGrid>
+							<div className={classes.half}>
+								<div className={classes.personaName}>
+									<Image src={Persona} className={classes.personaImage} />
+									<div>
+										<Heading>Sam</Heading>
+										<SubHeading>The Avid Gamer</SubHeading>
+									</div>
 								</div>
 							</div>
-						</div>
-						<div className={classes.half}>
-							<SubHeading>Goals</SubHeading>
-							<ul>
-								<li>
-									Improve skills at playing certain games and gauge that improvement
-								</li>
-								<li>
-									Have some competitive fun
-								</li>
-							</ul>
-							<SubHeading>Frustrations</SubHeading>
-							<ul>
-								<li>
-									Entering info quickly and easily
-								</li>
-								<li>
-									Mobile experience
-								</li>
-								<li>
-									Seeing change in data over time
-								</li>
-							</ul>
-							<SubHeading>Motivations</SubHeading>
-							<ul>
-								<li>
-									Winning
-								</li>
-								<li>
-									Improving skills
-								</li>
-								<li>
-									Prizes and bets
-								</li>
-							</ul>
-						</div>
-					</ItemGrid>
+							<div className={classes.half}>
+								<SubHeading>Goals</SubHeading>
+								<ul>
+									<li>
+										Improve skills at playing certain games and gauge that improvement
+									</li>
+									<li>
+										Have some competitive fun
+									</li>
+								</ul>
+								<SubHeading>Frustrations</SubHeading>
+								<ul>
+									<li>
+										Entering info quickly and easily
+									</li>
+									<li>
+										Mobile experience
+									</li>
+									<li>
+										Seeing change in data over time
+									</li>
+								</ul>
+								<SubHeading>Motivations</SubHeading>
+								<ul>
+									<li>
+										Winning
+									</li>
+									<li>
+										Improving skills
+									</li>
+									<li>
+										Prizes and bets
+									</li>
+								</ul>
+							</div>
+						</ItemGrid>
+					</div>
+					<Spacer height={3} />
+					<SubHeading>What are people using now?</SubHeading>
+					<Spacer />
+					<Body>
+						People who are tracking games now, are using tools as simple as the notes app so we have to make sure that using this app is just as easy. More serious gamers are using very complex tools like Challonge where leaderboards are hidden behind complicated UI.
+					</Body>
+					<Image src={Competitors} size="full" />
+				</ContentBlock>
+			</a>
+
+			{/* START - ARCH - START */}
+			<a name="architecture">
+				<div className={classes.sectionLabel}>
+					ARCHITECTURE
 				</div>
-				<Spacer height={3} />
-				<SubHeading>What are people using now?</SubHeading>
-				<Spacer />
-				<Body>
-					People who are tracking games now, are using tools as simple as the notes app so we have to make sure that using this app is just as easy. More serious gamers are using very complex tools like Challonge where leaderboards are hidden behind complicated UI.
-				</Body>
-				<Image src={Competitors} size="full" />
-			</ContentBlock>
-			<ContentBlock className={classes.blueBackground}>
-				<Heading>Building with a user-centered approach</Heading>
-				<Spacer />
-				<Spacer height={2} />
-				<Body>THE MAIN USER TASK</Body>
-				<SubHeading>
-					I just beat my friend at a game and&nbsp;
-					<span className={classes.highlight}>&nbsp;I need to&nbsp;</span>
-					&nbsp;submit this match and see how it affects my ranking
-				</SubHeading>
-				<Spacer />
-				<ItemGrid>
-					<div className={classes.half}>
-						<SubHeading>
-							Iterating on wireframes
-						</SubHeading>
-						<Spacer />
-						<Body>
-							Originally I wanted to lead new users to create a leaderboard before they could submit a match by disabling some CTAs and highlighting others. I quickly realized that I could let the user click where they wanted to if I could help navigate them to complete the necessary tasks in the back end.
-						</Body>
-					</div>
-					<Image src={WireDisabled} size="full" className={classes.half} />
-				</ItemGrid>
-				<SubHeading>Ajdusting for a user-centered experience</SubHeading>
-				<Spacer />
-				<Body>
-					I adjusted this flow so that before filling out the form to submit a match, users had to select a game. If there were no games, I could populate the form to create a game instead of blocking pathways that a user would want to take.
-				</Body>
-				<Image src={finalflow} size="full" />
-				<SubHeading>
-					Building functional prototypes
-				</SubHeading>
-				<Spacer />
-				<Body>
-					To test these flows I built out two prototypes
-				</Body>
-				<Image src={complexWire} size="full" />
-				<VideoPlayer
-					src="https://player.vimeo.com/video/448968438?loop=1"
-					title="Prototype Animation"
-					aspectRatio="1:1"
-				/>
-				<VideoPlayer
-					src="https://player.vimeo.com/video/448970796?loop=1"
-					title="Prototype Animation"
-					aspectRatio="1:1"
-				/>
-			</ContentBlock>
-			<ContentBlock>
-				<Heading>Creating a visual design system</Heading>
-				<Spacer />
-				<SubHeading>Creating a logo</SubHeading>
-				<Spacer />
-				<Body>
-					We developed a list of many names for for the platform and once we landed on Ladders we created multiple logo options below.
-				</Body>
-				<Image size="full" src={ELOAltLogo} />
-				<Spacer />
-				<ItemGrid>
-					<Image size="half" src={ELOLogoBlack} />
-					<Image size="half" src={ELOLogoYellow} />
-				</ItemGrid>
-				<Spacer height={2} />
-				<SubHeading>Cohesive UI Kit</SubHeading>
-				<Image src={UIKit} size="full" />
-			</ContentBlock>
-			<ContentBlock className={classes.whiteBlock}>
-				<Heading>Final UI Designs</Heading>
-				<Spacer />
-				<Image size="full" src={Visual1} />
-				<Image size="full" src={Visual2} />
-				<Spacer />
-			</ContentBlock>
-			<ContentBlock>
-				<Heading>Testing for Validation and Usability</Heading>
-				<Body>
-					I had each participant complete the tasks for both new and returning users. I observed them complete the tasks over Zoom.
-				</Body>
-				<Spacer />
-				<SubHeading>
-					100% of the users completed both tasks with no problems
-				</SubHeading>
-				<Spacer />
-				<ItemGrid>
-					<div className={classes.half}>
-						<SubHeading>
-							Multiple paths to completion
-						</SubHeading>
-						<Body>
-							I built the forms so that if a new user clicked the Submit a Match button and did not yet have any games set up they could be redirected to complete that form first. During testing, participants used both pathways to complete the task validating this choice.
-						</Body>
-					</div>
-					<Image src={testingScreenshot} className={classes.half} size="full" />
-				</ItemGrid>
-				<Spacer />
-				<SubHeading>
-					Too many options
-				</SubHeading>
-				<Body>
-					Initially I added a setting for volatility when users were setting up a ranking board. This setting was meant to affect how much each game changed the rankings. Most participants were a little confused about this setting so I ultimately decided to remove it or build it in to advanced settings later.
-				</Body>
-			</ContentBlock>
-			<ContentBlock className={classes.lightYellow}>
-				<Heading>Key Learnings</Heading>
-				<Spacer />
-				<SubHeading>
-					User Centered Approach to a Roadmap
-				</SubHeading>
-				<Body>
-					Working closely with developers on this project I had a tendency to lean into a developer or business mindset to planning the roadmap. I was creating priority between sign-up and login pages but shifted my mindset to think about the flows that were most necessary and valuable to the user.
-				</Body>
-				<Spacer />
-				<ItemGrid>
-					<div className={classes.half}>
-						<SubHeading>Coding Skills</SubHeading>
-						<Body>
-							As a designer with some coding skills working with a small team of 2 developers to build an entire app, I also put my coding skills to the test, styling and building simple elements in our Storybook using React.
-						</Body>
-					</div>
-					<Image src={ELOStorybook} className={classes.half} />
-				</ItemGrid>
-				<Spacer />
-				<SubHeading>End to End App</SubHeading>
-				<Body>
-					Creating something from scratch can be overwhelming at times but always rewarding. I found that focusing on what would be the most valuable to the user and testing those interactions helped me reign myself in from taking on too much right out of the gate.
-				</Body>
-			</ContentBlock>
+				<ContentBlock className={classes.blueBackground}>
+					<Heading>Building with a user-centered approach</Heading>
+					<Spacer />
+					<Spacer height={2} />
+					<Body>THE MAIN USER TASK</Body>
+					<SubHeading>
+						I just beat my friend at a game and&nbsp;
+						<span className={classes.highlight}>&nbsp;I need to&nbsp;</span>
+						&nbsp;submit this match and see how it affects my ranking
+					</SubHeading>
+					<Spacer />
+					<ItemGrid>
+						<div className={classes.half}>
+							<SubHeading>
+								Iterating on wireframes
+							</SubHeading>
+							<Spacer />
+							<Body>
+								Originally I wanted to lead new users to create a leaderboard before they could submit a match by disabling some CTAs and highlighting others. I quickly realized that I could let the user click where they wanted to if I could help navigate them to complete the necessary tasks in the back end.
+							</Body>
+						</div>
+						<Image src={WireDisabled} size="full" className={classes.half} />
+					</ItemGrid>
+					<SubHeading>Ajdusting for a user-centered experience</SubHeading>
+					<Spacer />
+					<Body>
+						I adjusted this flow so that before filling out the form to submit a match, users had to select a game. If there were no games, I could populate the form to create a game instead of blocking pathways that a user would want to take.
+					</Body>
+					<Image src={finalflow} size="full" />
+					<SubHeading>
+						Building functional prototypes
+					</SubHeading>
+					<Spacer />
+					<Body>
+						To test these flows I built out two prototypes
+					</Body>
+					<Image src={complexWire} size="full" />
+					<VideoPlayer
+						src="https://player.vimeo.com/video/448968438?loop=1"
+						title="Prototype Animation"
+						aspectRatio="1:1"
+					/>
+					<VideoPlayer
+						src="https://player.vimeo.com/video/448970796?loop=1"
+						title="Prototype Animation"
+						aspectRatio="1:1"
+					/>
+				</ContentBlock>
+			</a>
+
+			{/* START - BRAND - START */}
+			<a name="branding">
+				<div className={classes.sectionLabel}>
+					BRANDING
+				</div>
+				<ContentBlock>
+					<Heading>Creating a visual design system</Heading>
+					<Spacer />
+					<SubHeading>Creating a logo</SubHeading>
+					<Spacer />
+					<Body>
+						We developed a list of many names for for the platform and once we landed on Ladders we created multiple logo options below.
+					</Body>
+					<Image size="full" src={ELOAltLogo} />
+					<Spacer />
+					<ItemGrid>
+						<Image size="half" src={ELOLogoBlack} />
+						<Image size="half" src={ELOLogoYellow} />
+					</ItemGrid>
+					<Spacer height={2} />
+					<SubHeading>Cohesive UI Kit</SubHeading>
+					<Image src={UIKit} size="full" />
+				</ContentBlock>
+				<ContentBlock className={classes.whiteBlock}>
+					<Heading>Final UI Designs</Heading>
+					<Spacer />
+					<Image size="full" src={Visual1} />
+					<Image size="full" src={Visual2} />
+					<Spacer />
+				</ContentBlock>
+			</a>
+
+			{/* START - TEST - START */}
+			<a name="testing">
+				<div className={classes.sectionLabel}>
+					TESTING
+				</div>
+				<ContentBlock>
+					<Heading>Testing for Validation and Usability</Heading>
+					<Body>
+						I had each participant complete the tasks for both new and returning users. I observed them complete the tasks over Zoom.
+					</Body>
+					<Spacer />
+					<SubHeading>
+						100% of the users completed both tasks with no problems
+					</SubHeading>
+					<Spacer />
+					<ItemGrid>
+						<div className={classes.half}>
+							<SubHeading>
+								Multiple paths to completion
+							</SubHeading>
+							<Body>
+								I built the forms so that if a new user clicked the Submit a Match button and did not yet have any games set up they could be redirected to complete that form first. During testing, participants used both pathways to complete the task validating this choice.
+							</Body>
+						</div>
+						<Image src={testingScreenshot} className={classes.half} size="full" />
+					</ItemGrid>
+					<Spacer />
+					<SubHeading>
+						Too many options
+					</SubHeading>
+					<Body>
+						Initially I added a setting for volatility when users were setting up a ranking board. This setting was meant to affect how much each game changed the rankings. Most participants were a little confused about this setting so I ultimately decided to remove it or build it in to advanced settings later.
+					</Body>
+				</ContentBlock>
+			</a>
+
+			{/* START - LEARNINGS - START */}
+			<a name="learnings">
+				<div className={classes.sectionLabel}>
+					LEARNINGS
+				</div>
+				<ContentBlock className={classes.lightYellow}>
+					<Heading>Key Learnings</Heading>
+					<Spacer />
+					<SubHeading>
+						User Centered Approach to a Roadmap
+					</SubHeading>
+					<Body>
+						Working closely with developers on this project I had a tendency to lean into a developer or business mindset to planning the roadmap. I was creating priority between sign-up and login pages but shifted my mindset to think about the flows that were most necessary and valuable to the user.
+					</Body>
+					<Spacer />
+					<ItemGrid>
+						<div className={classes.half}>
+							<SubHeading>Coding Skills</SubHeading>
+							<Body>
+								As a designer with some coding skills working with a small team of 2 developers to build an entire app, I also put my coding skills to the test, styling and building simple elements in our Storybook using React.
+							</Body>
+						</div>
+						<Image src={ELOStorybook} className={classes.half} />
+					</ItemGrid>
+					<Spacer />
+					<SubHeading>End to End App</SubHeading>
+					<Body>
+						Creating something from scratch can be overwhelming at times but always rewarding. I found that focusing on what would be the most valuable to the user and testing those interactions helped me reign myself in from taking on too much right out of the gate.
+					</Body>
+				</ContentBlock>
+			</a>
 		</PageWrapper>
 	)
 }
