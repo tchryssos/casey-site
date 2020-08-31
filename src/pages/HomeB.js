3c29f2bc2c97cf9d02f33ba389c298d50005f45b
Casey Bradford <caseyebradford@gmail.com>
2020-08-31 15:28:16 -0400

    home b images

--- src/pages/HomeB.js
+++ src/pages/HomeB.js
@@ -1,62 +1,75 @@
 import React from 'react'
 import { createUseStyles } from 'react-jss'
-import {
-	MD_MIN_STRING,
-} from 'constants/styles/breakpoints'
 
 import PageWrapper from 'components/PageWrapper'
 import ContentBlock from 'components/ContentBlock'
 import ProjectSummary from 'components/ProjectSummary'
+import Heading from 'components/Typography/Heading'
+import Body from 'components/Typography/Body'
+import Spacer from 'components/Spacer'
 
-import TestBlob from 'static/images/HomeB/testblob.png'
+import MHHBlob from 'static/images/HomeB/sq-01.png'
+import FISBlob from 'static/images/HomeB/sq-02.png'
+import ELOBlob from 'static/images/HomeB/sq-03.png'
+import ListenJayBlob from 'static/images/HomeB/sq-04.png'
+import MirrorBlob from 'static/images/HomeB/sq-05.png'
 
 const useStyles = createUseStyles({
-
+	aboutMeHeader: {
+		textAlign: 'center',
+		backgroundColor: 'white',
+	},
 })
 
 export default () => {
 	const classes = useStyles()
 	return (
 		<PageWrapper>
+			<ContentBlock className={classes.aboutMeHeader}>
+				<Heading>
+					I am a product designer with an extensive background in brand and marketing design.
+				</Heading>
+				<Spacer />
+				<Body>
+					Read more about me here and view my most recent work below
+				</Body>
+			</ContentBlock>
 			<ContentBlock>
 				<ProjectSummary
 					heading="Men&apos;s Health Houston Website Redesign"
 					body="Updating the branding and website design for a men&apos;s health clinic"
 					tag="DESIGNLAB CASE STUDY"
-					imageSrc={TestBlob}
+					imageSrc={MHHBlob}
 					link=""
 				/>
 				<ProjectSummary
 					heading="Flatiron School Marketing"
 					body="Managing a creative team working on everything including out-of-home, video, organic and paid social"
 					tag="Marketing Design Leadership"
-					imageSrc={TestBlob}
+					imageSrc={FISBlob}
 					link=""
 				/>
 				<ProjectSummary
 					heading="End to End App Design for a Gaming Tool"
 					body="Building a tool for people playing games to track their ranking amongst friends"
 					tag="DESIGNLAB CASE STUDY"
-					imageSrc={TestBlob}
+					imageSrc={ELOBlob}
 					link=""
 				/>
 				<ProjectSummary
 					heading="ListenJay Quote Creation Feature"
 					body="Adding a feature to a podcast discovery platform"
 					tag="DESIGNLAB CASE STUDY"
-					imageSrc={TestBlob}
+					imageSrc={ListenJayBlob}
 					link=""
 				/>
 				<ProjectSummary
 					heading="Ecommerce Website Design"
 					body="DesignLab case study project focused on a clothing ecommerce site"
 					tag="DesignLab Case Study"
-					imageSrc={TestBlob}
+					imageSrc={MirrorBlob}
 					link=""
 				/>
-				<div>
-					About Me
-				</div>
 			</ContentBlock>
 		</PageWrapper>
 	)
