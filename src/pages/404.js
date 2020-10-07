2b817edd73f2616319280fd083e95922073cc065
Casey Bradford <caseyebradford@gmail.com>
2020-10-06 20:32:29 -0400

    404 link

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,17 +1,41 @@
 import React from 'react'
+import { createUseStyles } from 'react-jss'
+import { NavLink } from 'react-router-dom'
 
 import PageWrapper from 'components/PageWrapper'
 import ContentBlock from 'components/ContentBlock'
-import Heading from 'components/Typography/Heading'
+import SubHeading from 'components/Typography/SubHeading'
 
-const FourOhFour = () => (
-	<PageWrapper>
-		<ContentBlock>
-			<Heading>
-				404
-			</Heading>
-		</ContentBlock>
-	</PageWrapper>
-)
+const useStyles = createUseStyles({
+	button: {
+		backgroundColor: '#346c63',
+		padding: 8,
+		color: 'white',
+		display: 'inline-flex',
+		marginTop: 24,
+		textDecoration: 'none',
+		fontWeight: 600,
+		'&:hover': {
+			backgroundColor: '#4e7fff',
+		},
+	},
+	cAligned: {
+		textAlign: 'center',
+	},
+})
 
-export default FourOhFour
+export default () => {
+	const classes = useStyles()
+	return (
+		<PageWrapper>
+			<ContentBlock className={classes.cAligned}>
+				<SubHeading>
+					Sorry, we can&apos;t find that page.
+				</SubHeading>
+				<NavLink className={classes.button} to="/" exact>
+					Back to Home
+				</NavLink>
+			</ContentBlock>
+		</PageWrapper>
+	)
+}
